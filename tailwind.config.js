eabd50d63170c4d6603129dbb1bb3e183541a45e
sheikh295 <sheikh295@users.noreply.github.com>
2023-07-21 20:40:04 +0500

    initial build (responsiveness)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,31 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      '3xs': '300px',
+      // => @media (min-width: 300px) { ... }
+
+      '2xs': '400px',
+      // => @media (min-width: 400px) { ... }
+
+      'xs': '520px',
+      // => @media (min-width: 520px) { ...}
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {},
   },
   plugins: [
